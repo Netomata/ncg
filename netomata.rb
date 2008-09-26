6e3fb70c5cb09d482ca1d13c0689fcb381f97b93
brent <brent@e280301d-f36b-4a17-bc5a-6e1e100efe5d>
2008-09-26 20:39:16 +0000

    Changed name from "Element" to "Node".  -Brent

--- netomata.rb
+++ netomata.rb
@@ -18,21 +18,21 @@ end
 
 class Netomata
 
-    class Element < Dictionary
+    class Node < Dictionary
 
 	def initialize
 	    super()
 	end
 
-	# return true if keys/values of this element match
+	# return true if keys/values of this node match
 	# an expression of the form:
 	# 	(key=value[,key2=value2 ...])
-	def element_match?(m)
+	def node_match?(m)
 	    # strip parentheses and split into parts
 	    m.gsub(/[()]/,"").split(/,/).each { |mp|
 		k,v = mp.split(/=/)
 		if (k.nil? or v.nil?) then
-		    raise ArgumentError, "invalid syntax for element_match term"
+		    raise ArgumentError, "invalid syntax for node_match term"
 		end
 		if (! self.has_key?(k)) then
 		    return false;
@@ -45,25 +45,25 @@ class Netomata
 	    return true;
 	end
 
-	# return array of subelements that match expression
-	def subelement_matches(m)
+	# return array of subnodes that match expression
+	def subnode_matches(m)
 	    ra = Array.new
 	    self.each { |k,v|
-		if v.element_match?(m) then
+		if v.node_match?(m) then
 		    ra << v
 		end
 	    }
 	    return ra
 	end
 
-	# return single match of subelements that match expression, or
+	# return single match of subnodes that match expression, or
 	# raise an error if more than 1
-	def subelement_match_one(m)
-	    ra = self.subelement_matches(m)
+	def subnode_match_one(m)
+	    ra = self.subnode_matches(m)
 	    case ra.length
 		when 0 then return nil
 		when 1 then return ra[0]
-		else raise RuntimeError, "matched more than 1 subelement"
+		else raise RuntimeError, "matched more than 1 subnode"
 	    end
 	end
 
@@ -76,7 +76,7 @@ class Netomata
 		# if r is nil, then there was no "!" in the key
 		if (l.match(/^\(.*\)$/)) then
 		    # l contains a "(...)" selector; dereference
-		    return self.subelement_match_one(l)
+		    return self.subnode_match_one(l)
 		else
 		    return super(l)
 		end
@@ -84,11 +84,11 @@ class Netomata
 		if (l.match(/^\(.*\)$/)) then
 		    # l contains a "(...)" selector; dereference
 		    # if dereferencing l returns nil, then we're done
-		    result = self.subelement_match_one(l)
+		    result = self.subnode_match_one(l)
 		    if result.nil? then
 			return nil
 		    else
-			return self.subelement_match_one(l)[r]
+			return self.subnode_match_one(l)[r]
 		    end
 		else
 		    return super(l)[r]
@@ -112,12 +112,12 @@ class Netomata
 		    if (l.match(/^\(.*\)$/)) then
 			# l is a match statement, but isn't matching anything
 			raise ArgumentError,
-			    "no elements found with key \"#{l}\""
+			    "no nodes found with key \"#{l}\""
 		    end
-		    self[l] = Netomata::Element.new
+		    self[l] = Netomata::Node.new
 		end
-		if (self[l].class != Netomata::Element) then
-		    raise ArgumentError, "key \"#{l}\" already defined, but value is not of type Netomata::Element"
+		if (self[l].class != Netomata::Node) then
+		    raise ArgumentError, "key \"#{l}\" already defined, but value is not of type Netomata::Node"
 		end
 		self[l][r]=v
 	    end
@@ -164,7 +164,7 @@ class Netomata
     end
 end
 
-fa = Netomata::Element.new
+fa = Netomata::Node.new
 fa.import(open("vlans"), "!vlans")
 pp(fa)
 pp(fa["!vlans"])

47e35a16d8a37bc127d89d63f126ec30335379a4
brent <brent@e280301d-f36b-4a17-bc5a-6e1e100efe5d>
2009-01-15 04:20:09 +0000

    Added '-V' flag to print version number and exit.

--- lib/netomata.rb
+++ lib/netomata.rb
@@ -5,17 +5,24 @@
 # disclaimers, and license terms (GPL v2.0 or alternative).
 
 require 'rubygems'
+require 'erb'
 require 'facets'
 require 'facets/dictionary'
 require 'ipaddr'
-require 'pp'
-require 'breakpoint'
-require 'ruby-debug'
+begin	# rescue block; no big deal if these aren't found, since
+    	# they're only used for debugging
+    require 'pp'
+    require 'breakpoint'
+    require 'ruby-debug'
+rescue LoadError
+    # 
+end
 
 class Netomata
     # stub class, for other files to add subclasses to
 end
 
+require 'netomata/version'
 require 'netomata/utilities'
 require 'netomata/node'
 require 'netomata/template'

d5b6175667129c680fc1817f39bc4ff9ef761c3b
brent <brent@e280301d-f36b-4a17-bc5a-6e1e100efe5d>
2009-04-10 18:00:59 +0000

    Added unit tests for template_arg() and template_arg_node_key() methods

--- test/test_20_utilities.rb
+++ test/test_20_utilities.rb
@@ -21,7 +21,7 @@ require 'netomata'
 
 require 'test/unit'
 
-class UtilitiesTest < Test::Unit::TestCase
+class UtilitiesTest_1 < Test::Unit::TestCase
     # First set of tests for Netomata::Utilities
     def setup
 	$debug = false
@@ -78,5 +78,70 @@ class UtilitiesTest < Test::Unit::TestCase
 	assert_equal "a/b/d/e", @n.relative_filename("a/b/c", "d/e")
 	assert_equal "/d", @n.relative_filename("/a/b/c", "/d")
 	assert_equal "/d/e", @n.relative_filename("/a/b/c", "/d/e")
+    end
+end
+
+class UtilitiesTest_2 < Test::Unit::TestCase
+    # Second set of tests for Netomata::Utilities
+
+    include Netomata::Utilities
+    include Netomata::Utilities::ClassMethods
+
+    def setup
+        $debug = false
+        @node = Netomata::Node.new
+        @node["!n1!k_n1a1"] = "v_n1a1"
+        @node["!n1!k_n1a2"] = "v_n1a2"
+        @node["!n1!k_n1a3"] = "v_n1a3"
+        @node["!n1!k_common_k"] = "v_common_n1"
+        @node["!n1!k_common_kv"] = "v_common_kv"
+        @node["!n1!k_common_n!k_common_n_kv"] = "v_common_kv"
+        @node["!n2!k_n2a1"] = "v_n2a1"
+        @node["!n2!k_n2a2"] = "v_n2a2"
+        @node["!n2!k_n2a3"] = "v_n2a3"
+        @node["!n2!k_common_k"] = "v_common_n2"
+        @node["!n2!k_common_kv"] = "v_common_kv"
+        @node["!n2!k_common_n!k_common_n_kv"] = "v_common_kv"
+        @node["!n1!n11!k_n11"] = "v_n11"
+        @node["!n1!n12!k_n12"] = "v_n12"
+        @node["!n1!n13!k_n13"] = "v_n13"
+        @node["!n1!n11!n111!k1111"] = "v1111"
+        @node["!n1!n11!n111!k1112"] = "v1112"
+        @node["!n1!n11!n111!k1113"] = "v1113"
+        @node["!n1!n11!n112!k1121"] = "v1121"
+        @node["!n1!n11!n112!k1122"] = "v1122"
+        @node["!n1!n11!n112!k1123"] = "v1123"
+        @node["!n1!n11!n113!k1131"] = "v1131"
+        @node["!n1!n11!n113!k1132"] = "v1132"
+        @node["!n1!n11!n113!k1133"] = "v1133"
+        @node["!n3!(+)"] = "first"
+        @node["!n3!(+)"] = "second"
+        @node["!n3!(+)"] = "third"
+        @node["!n3!(+)"] = "last"
+    end
+
+    def test_template_arg
+	assert template_arg("@node")
+	assert_nothing_raised { template_arg("@node") }
+	assert_raise(ArgumentError) { template_arg("@undefined") }
+	assert_raise(ArgumentError) { template_arg("@node", String) }
+    end
+
+    def test_template_arg_node_key
+	assert template_arg_node_key("@node", "!n1!k_n1a3")
+	assert_nothing_raised { template_arg_node_key("@node", "!n1!k_n1a3") }
+
+	assert template_arg_node_key("@node", "!n1!k_n1a3", String)
+	assert_nothing_raised { 
+	    template_arg_node_key("@node", "!n1!k_n1a3", String) }
+
+	assert_raise(ArgumentError) {
+	    template_arg_node_key("@undefined", "foo") }
+
+	assert_raise(ArgumentError) {
+	    template_arg_node_key("@node", "foo") }
+
+	assert_raise(ArgumentError) {
+	    template_arg_node_key("@node", "n1!n11") }
     end
 end
